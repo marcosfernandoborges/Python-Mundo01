3e8621a8a3444f39c075ebd1f3f4e47b9e430765
Marcos Fernando Borges <bmarcosfernando@gmail.com>
2020-09-24 18:08:56 -0300

    Desafio02-Python
    Segundo Desafio Curso em Video - Gustavo Guanabara

---
+++ desafio02.py
@@ -0,0 +1,6 @@
+print('-------- DESAFIO 02 --------')
+dia=input('INFORME O DIA DE NASCIMENTO: ')
+mes=input('INFORME O MES DE NASCIMENTO: ')
+ano=input('INFORME O ANO DE NASCIMENTO: ')
+
+print('DATA DE NASCIMENTO: ' , dia , '|' , mes , '|' , ano)
