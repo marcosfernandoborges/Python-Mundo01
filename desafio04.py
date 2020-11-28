93148e632ca2ed1e81e9669b75e4a3bcb378f70f
Marcos Fernando Borges <bmarcosfernando@gmail.com>
2020-11-28 00:17:49 -0300

    Desafio04
    Desafio 04 - Verificação de strings (.is)

---
+++ desafio04.py
@@ -0,0 +1,9 @@
+n = input('Digite algo: ')
+print(n)
+print('1)- A informação digitada é numérica?' , n.isnumeric())
+print('2)- A informação digitada é letra? ' , n.isalpha())
+print('3)- A informação digitada é Alfanumérica? ' , n.isalnum())
+print('4)- A informação esta toda em letra MAIÚSCULA? ' , n.isupper())
+
+
+
